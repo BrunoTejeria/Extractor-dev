feb12ff32c327c29846ef127af99945ed4ab0a7f
blad64 <brunotejeria@hotmail.com>
2023-10-07 20:42:15 -0300

    organizacion del proyecto

---
+++ release/0.1/src/modules/m_logic/lines.py
@@ -0,0 +1,14 @@
+class Lines:
+
+    #lineas_cantidad = Lines(file=text_file)
+    #lineas_cantidad.count()
+
+    def __init__(self,file):
+        with open(file, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+            file.close()
+        self.lines = lines
+
+    def count(self):
+        total_lines = sum(1 for line in self.lines)
+        self.count = total_lines
