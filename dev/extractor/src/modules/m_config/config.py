b32bace00dd359b2d262b8da2bab8e0ab306cad3
blad64 <brunotejeria@hotmail.com>
2023-10-10 19:37:54 -0300

    cambios en config y json bug en la importacion de json desde modulo config

--- dev/extractor/src/modules/m_config/config.py
+++ dev/extractor/src/modules/m_config/config.py
@@ -1,5 +1,17 @@
-import json
+from ..m_json.json import Json
 
-class Config:
-    def __init__(self):
-        pass
+path = 'extractor/etc/config.json'
+
+class Config(Json):
+    def __init__(self, json_path, mode_='r'):
+        super().__init__(json_path, mode_)
+
+    def get_content(self):
+        return self.read()
+
+# Crear una instancia de la clase Config
+config = Config(path)
+# Leer el contenido del archivo JSON
+data = config.get_json_content()
+
+print(data)
