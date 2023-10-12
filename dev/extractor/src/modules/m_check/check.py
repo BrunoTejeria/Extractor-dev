165c20306d97cd53c52fad5c3fb2e59b96637bca
blad64 <brunotejeria@hotmail.com>
2023-10-12 20:45:13 -0300

    - Codigo base para multi check realizado

--- dev/extractor/src/modules/m_check/check.py
+++ dev/extractor/src/modules/m_check/check.py
@@ -1,61 +1,62 @@
 from ..m_file.file import File
 from ..m_config.config import Config
 from tqdm import tqdm
+import json
 # Ruta del archivo de configuracion
 config_path = 'extractor/etc/config.json'
-
-class ConfigValues(Config):
+class Check(File, Config):
     def __init__(self):
-        config_json = Config(config_path)
-        config = config_json.read()
-        self.url =  config[1]['url']
+        config_json_class = Config(config_path)
+        config = config_json_class.read()
+        
         self.file_search =  config[0]['textFile']['textSearch']
         self.file_result = config[0]['textFile']['textResult']
-
-
-class Check(File, ConfigValues):
-    def __init__(self):
-
-        # Heredar de clase ConfigValuesd
-        ConfigValues.__init__(self)
-
-        # Heredar de clase File
         File.__init__(self, self.file_search, self.file_result)
         File.read(self)
+        self.url = config
 
 
     def process_lines(self):
         result_lines = []
-
-        with tqdm(total=len(self.lines), desc="Procesando", unit=" línea", colour='blue') as progress_bar:
-            for line in self.lines:
-                progress_bar.update(1)
-                if line != '\n':
-                    if self.url in line:
-                        result = ''
-                        try:
-                            post_result = line.find(':', line.find(':') + 1)
-                            if self.url in line[:post_result]:
+        
+        for site in self.url[1]['url']:
+            file_result = self.file_result
+            print(site)
+            with tqdm(total=len(self.lines), desc="Procesando", unit=" línea", colour='blue') as progress_bar:
+                for search in self.url[1]['url'][site]:
+                    url = self.url[1]['url'][site][search]
+                    print(url)
+                    for line in self.lines:
+                        progress_bar.update(1)
+                        if line != '\n':
+                            if url in line:
+                                print(line)
+                                result = ''
                                 try:
-                                    result = line[line.index(' ') + 1:]
-                                except ValueError:
                                     post_result = line.find(':', line.find(':') + 1)
-                                    result = line[post_result + 1:]
-                        except ValueError:
-                                try:
-                                    result = line[line.index(' ') + 1:]
+                                    if url in line[:post_result]:
+                                        try:
+                                            result = line[line.index(' ') + 1:]
+                                        except ValueError:
+                                            post_result = line.find(':', line.find(':') + 1)
+                                            result = line[post_result + 1:]
+                                        print(result)
                                 except ValueError:
-                                    pass
-                        if result != '':
-                            result_lines.append(result)
+                                        try:
+                                            result = line[line.index(' ') + 1:]
+                                        except ValueError:
+                                            pass
+                                if result != ''and not 'UNKNOWN' in result and not result in result_lines:
+                                    result_lines.append(result)
 
-            with open(self.file_result, 'a',) as file_:
-                for line in result_lines:
-                    try:
-                        file_.write(line)
-                    except:
-                        pass
-
+                    with open(file_result, 'a',) as file_:
+                        a = f"############{site}############'\n"
+                        file_.write(a)
+                        for line in result_lines:
+                            try:
+                                file_.write(line)
+                            except:
+                                pass
 
         return result_lines
 
