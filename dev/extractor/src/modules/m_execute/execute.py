163318945ac9228fa2d1598fa861a2f0c85b22df
blad64 <brunotejeria@hotmail.com>
2023-10-27 12:37:59 -0300

    - Proceso de desarrollo de desabilitacion y habilitacion de opciones

--- dev/extractor/src/modules/m_execute/execute.py
+++ dev/extractor/src/modules/m_execute/execute.py
@@ -59,8 +59,33 @@ table_config = \
 
 
 
-
+# Funcion para configurar
 def config():
+    # Funcion para configurar sitios
+    def site():
+        choices_sites = []
+        for site in search.config[1]['site']:
+            choices_sites.append(site)
+
+        print("Selecciona el sitio el cual quires configurar:")
+        index = 1
+        for site in choices_sites:
+            print(f'[green]{index}.[/green] [red]{site}[/red]')
+            index += 1
+        selected_option = Prompt.ask('Selecciona una opción (ingresa 0 para salir):', choices=list(range(1, len(choices_sites)+1)), show_choices=True)
+        selected_site = choices_sites[int(selected_option)-1]
+        print(int(selected_option)-1)
+        print(int(selected_option))
+
+
+    # Funcion para configurar urls
+    def text_read():
+        pass
+    # Funcion para configurar urls
+    def text_write():
+        pass
+    site()
+
     print(f'[green]{text}[/green]')
     while True:
         console.print(table_config)
@@ -73,6 +98,7 @@ def config():
 
         elif selected_option == "2":
             console.print("Has seleccionado 'Configuración'", style="bold green")
+            config()
 
         elif selected_option == "3":
             console.print("Has seleccionado 'Opción 3'", style="bold green")
