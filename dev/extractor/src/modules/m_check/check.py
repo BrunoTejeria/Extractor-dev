23e4670c8d9cf26252e2528e89af3b4e7bc8c2d1
blad64 <brunotejeria@hotmail.com>
2023-10-28 13:00:08 -0300

    - Cambios en el chequeo y optimizacion

--- dev/extractor/src/modules/m_check/check.py
+++ dev/extractor/src/modules/m_check/check.py
@@ -8,6 +8,7 @@ try:
     from rich.console import Console
     from tqdm import tqdm
     import json
+    import os
 except ImportError:
     pass
 
@@ -19,12 +20,15 @@ result_path = 'extractor/etc/data/processed_data/'
 console = Console()
 
 # Definición de la clase Check, que hereda de las clases File y Config
+# Importar las clases File y Config
 class Check(File, Config):
     def __init__(self):
+        # Leer la configuración
         self.config = Config(config_path).read()
 
         # Llamar al constructor de la clase Config
         Config.__init__(self, config_path)
+
         # Obtener rutas de archivos desde la configuración
         self.file_search =  self.config[0]['textFile']['textSearch']
 
@@ -32,45 +36,56 @@ class Check(File, Config):
         File.__init__(self, self.file_search)
         File.read(self)
 
-
-
-
     # Método para procesar líneas del archivo
     def process_lines(self):
+        # Obtener las líneas del archivo
         file_lines = self.lines
         count = 1
 
-
         # Iterar a través de las URL en la configuración
         for site in self.config[1]['site']:
+            # Objetener el tipo que es el resutado para saber en que carpeta hay que guardarlo
+            search_type = self.config[1]['site'][site]['genere']
+
             # Crear un archivo para los resultados o resetearlo si ya existe
-            file = open(f'{result_path}result_{site}.txt', 'w')
+            try:
+                file = open(f'{result_path}{search_type}/{site}.txt', 'w')
+                file_path = f'{result_path}{search_type}/{site}.txt'
+            except FileNotFoundError:
+                os.mkdir(f'{result_path}/{search_type}')
+                file = open(f'{result_path}{search_type}/{site}.txt', 'w')
+                file_path = f'{result_path}{search_type}/{site}.txt'
+            except:
+                os.mkdir(f'{result_path}/others')
+                file = open(f'{result_path}/others/{site}.txt', 'w')
+                file_path = f'{result_path}/others/{site}.txt'
 
             # imprimir en que se esta buscando
             console.print \
             (f'''
             [bold red]{site}[/bold red]
             ''')
+
+            # Obtener el tipo de búsqueda (mail, user, all)
             if self.config[1]['site'][site]['type'] == 'mail':
                 userType = 'mail'
             elif  self.config[1]['site'][site]['type'] == 'user':
                 userType = 'user'
             else:
                 userType = 'all'
-            
-           
+
+            # Inicializar listas para los resultados
             results = []
             result_lines = []
-            
+
             # Crear una barra de progreso con tqdm
             with tqdm(total=len(file_lines * len(self.config[1]['site'][site]["searchSites"])), desc="Procesando", unit="línea", colour='green') as progress_bar:
                 # Iterar a través de las URL y búsquedas en la configuración
                 for searchSite in self.config[1]["site"][site]["searchSites"]:
-                    
                     url = self.config[1]['site'][site]["searchSites"][searchSite]
 
                     # Iterar a través de las líneas del archivo de búsqueda
-                    for line in file_lines:
+                    for line in file_lines[1000000:]:
                         result = ''
 
                         progress_bar.update(1)
@@ -84,32 +99,46 @@ class Check(File, Config):
 
             # Iterar a través de los resultados
             results_first_part = []
-            for result in result_lines:
-                try:
-                    result_first_part = result.split(':')[0]
-                    if not "UNKNOWN" in result:
-                        if not result_first_part in results_first_part and len(result) <= 64:
-                            if userType == 'mail':
-                                # Comprobar si el resultado contiene una @
-                                if '@' in result:
-                                    # Añadir el resultado a la lista de resultado
-                                    results_first_part.append(result.split(':')[0])
-                                    results.append(result)
-                            else:
-                                results_first_part.append(result.split(':')[0])
-                                results.append(result)
-                except ValueError as e:
-                    print(e)
+            print('\n')
+            with tqdm(total=len(result_lines), desc="Chequeando", unit="línea", colour='blue', ) as progress_bar:
+                if userType == 'mail':
+                    for result in result_lines:
+                        try:
+                            if not "UNKNOWN" in result:
+                                if len(result) <= 64:
+                                    result_first_part = result.split(':')[0]
+                                    if not result_first_part in results_first_part:
+                                        # Comprobar si el resultado contiene una @
+                                        if '@' in result_first_part:
+                                            # Añadir el resultado a la lista de resultado
+                                            results_first_part.append(result.split(':')[0])
+                                            results.append(result)
+                        except ValueError as e:
+                            print(e)
+                        progress_bar.update(1)
+                else:
+                    for result in result_lines:
+                        try:
+                            if not "UNKNOWN" in result:
+                                result_first_part = result.split(':')[0]
+                                if len(result) <= 64:
+                                    if not result_first_part in results_first_part:
+                                        results_first_part.append(result_first_part)
+                                        results.append(result)
+                        except ValueError as e:
+                            print(e)
+                        progress_bar.update(1)
 
             # Escribir los resultados en un archivo
-            with open(f'{result_path}result_{site}.txt', 'a',) as file_:
+            with open(file=file_path, mode='a',) as file_:
                     for result in results:
                         if result != '\n':
                             try:
                                 file_.write(result)
                             except:
                                 continue
-        console.print("[bold green]\nResultados totales: [/bold green]" + str(len(results)))
+            console.print("[bold green]\nResultados totales: [/bold green]" + str(len(results)))
+            print('\n \n')
 
         # Devolver la lista de resultados
         return result_lines
