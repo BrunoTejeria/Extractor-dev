499e5c6c54787297f628bd274119689fed0bbf6b
blad64 <brunotejeria@hotmail.com>
2023-11-22 15:44:09 -0300

    - Importantes optimizaciones

--- dev/extractor/src/modules/m_check/check.py
+++ dev/extractor/src/modules/m_check/check.py
@@ -11,6 +11,8 @@ try:
     import json
     import os
     import numpy as np
+    import time
+    import logging
 except ImportError:
     pass
 
@@ -72,31 +74,43 @@ class Check(File, Config):
             userType = self.config[1]['site'][site]['type']
 
             # Inicializar listas para los resultados
-            results: numpy.core = np.array([])
-            result_lines: numpy.core = np.array([])
+            results: list = []
+            result_lines: list = []
 
             # Crear una barra de progreso con tqdm
+            
             with tqdm(total=len(file_lines * len(self.config[1]['site'][site]["searchSites"])), desc="Procesando", unit="línea", colour='green', unit_scale=True) as progress_bar:
                 # Iterar a través de las URL y búsquedas en la configuración
                 for searchSite in self.config[1]["site"][site]["searchSites"]:
                     url: dict = self.config[1]['site'][site]["searchSites"][searchSite]
 
                     # Iterar a través de las líneas del archivo de búsqueda
+                    result: str = ''
                     for line in file_lines:
-                        result: str = ''
 
+                        # Actualizar la barra de progreso
                         progress_bar.update(1)
 
-                        # Comprobar si la URL está en la línea
-                        if url in line:
-                            post_result: str = line.find(':', line.find(':') + 1)
-                            if url in line[:post_result]:
-                                result = line[post_result + 1:]
-                            result_lines: numpy = np.append(result_lines, result)
+                        post_result: str = line.find(':', line.find(':') + 1)
 
-            # Iterar a través de los resultados
-            results_first_part: numpy.core = np.array([])
+                        # Comprobar si la URL está en la línea
+                        if url in line[:post_result]:
+                            result = line[post_result + 1:]
+                            """
+                            Aquí se realiza la operación de agregar el resultado a la lista result_lines.
+                            Esta operación puede ser lenta debido a que se utiliza la función np.append() en cada iteración del bucle.
+                            Para mejorar el rendimiento, se recomienda utilizar una lista estándar de Python en lugar de un array de numpy.
+                            Esto se debe a que la función np.append() crea una nueva matriz en cada llamada, lo que puede ser ineficiente en términos de memoria y tiempo de ejecución.
+                            Además, se puede considerar utilizar una comprensión de lista para agregar los resultados a la lista result_lines, ya que es más eficiente que la función np.append().
+                            """
+                            result_lines.append(result)
+
+
+
+            results_first_part: list = []
             print('\n')
+
+            # Crear una barra de progreso con tqdm
             with tqdm(total=len(result_lines), desc="Chequeando", unit="línea", colour='blue', unit_scale=True) as progress_bar:
                 if userType == 'mail':
                     for result in result_lines:
@@ -107,8 +121,8 @@ class Check(File, Config):
                                     # Comprobar si el resultado contiene una @
                                     if '@' in result_first_part:
                                         # Añadir el resultado a la lista de resultado
-                                        results_first_part: numpy = np.append(results_first_part, result_first_part)
-                                        results: numpy = np.append(results, result)
+                                        results_first_part.append(result_first_part)
+                                        results.append(result)
                         except ValueError as e:
                             print(e)
                         progress_bar.update(1)
@@ -119,8 +133,8 @@ class Check(File, Config):
                                 if len(result) <= 64:
                                     result_first_part: str = result.split(':')[0]
                                     if not result_first_part in results_first_part:
-                                            results_first_part: numpy = np.append(results_first_part, results_first_part)
-                                            results: numpy = np.append(results, result)
+                                            results_first_part.append(results_first_part)
+                                            results.append(result)
                         except ValueError as e:
                             print(e)
                         progress_bar.update(1)
