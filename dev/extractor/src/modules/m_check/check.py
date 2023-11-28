de2ca03fc89d659a3132d4f7b6ac0f688095d94d
blad64 <brunotejeria@hotmail.com>
2023-11-28 14:26:37 -0300

    - Optimizacion en chequeo de resultados

--- dev/extractor/src/modules/m_check/check.py
+++ dev/extractor/src/modules/m_check/check.py
@@ -78,7 +78,7 @@ class Check(File, Config):
             result_lines: list = []
 
             # Crear una barra de progreso con tqdm
-            
+
             with tqdm(total=len(file_lines * len(self.config[1]['site'][site]["searchSites"])), desc="Procesando", unit="línea", colour='green', unit_scale=True) as progress_bar:
                 # Iterar a través de las URL y búsquedas en la configuración
                 for searchSite in self.config[1]["site"][site]["searchSites"]:
@@ -96,50 +96,37 @@ class Check(File, Config):
                         # Comprobar si la URL está en la línea
                         if url in line[:post_result]:
                             result = line[post_result + 1:]
-                            """
-                            Aquí se realiza la operación de agregar el resultado a la lista result_lines.
-                            Esta operación puede ser lenta debido a que se utiliza la función np.append() en cada iteración del bucle.
-                            Para mejorar el rendimiento, se recomienda utilizar una lista estándar de Python en lugar de un array de numpy.
-                            Esto se debe a que la función np.append() crea una nueva matriz en cada llamada, lo que puede ser ineficiente en términos de memoria y tiempo de ejecución.
-                            Además, se puede considerar utilizar una comprensión de lista para agregar los resultados a la lista result_lines, ya que es más eficiente que la función np.append().
-                            """
                             result_lines.append(result)
 
 
 
-            
+
             print('\n')
 
+
+
+            results_first_part = set()
+
+            # Seleccionar la condición común para ambos casos fuera del bucle
+            common_condition = lambda result: "UNKNOWN" not in result and len(result) <= 64
             # Crear una barra de progreso con tqdm
             with tqdm(total=len(result_lines), desc="Chequeando", unit="línea", colour='blue', unit_scale=True) as progress_bar:
-                results_first_part: list = []
-                if userType == 'mail':
-                    for result in result_lines:
-                        try:
-                            if not "UNKNOWN" in result:
-                                if len(result) <= 64:
-                                    result_first_part: str = result.split(':')[0]
-                                    if result_first_part not in results_first_part:
-                                        # Comprobar si el resultado contiene una @
-                                        if '@' in result_first_part:
-                                            # Añadir el resultado a la lista de resultado
-                                            results_first_part.append(result_first_part)
-                                            results.append(result)
-                        except ValueError as e:
-                            print(e)
-                        progress_bar.update(1)
-                else:
-                    for result in result_lines:
-                        try:
-                            if not "UNKNOWN" in result:
-                                if len(result) <= 64:
-                                    result_first_part: str = result.split(':')[0]
-                                    if not result_first_part in results_first_part:
-                                            results_first_part.append(result_first_part)
-                                            results.append(result)
-                        except ValueError as e:
-                            print(e)
-                        progress_bar.update(1)
+                for result in result_lines:
+                    try:
+                        result_first_part = result.split(':')[0]
+
+                        if common_condition(result):
+                            if userType == 'mail' and '@' in result_first_part:
+                                results_first_part.add(result_first_part)
+                                results.append(result)
+                            elif userType != 'mail':
+                                results_first_part.add(result_first_part)
+                                results.append(result)
+
+                    except ValueError as e:
+                        print(e)
+
+                    progress_bar.update(1)
 
             # Escribir los resultados en un archivo
             with open(file=file_path, mode='a',) as file_:
